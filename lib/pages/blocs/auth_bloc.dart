7ce8d0c59f6731ea28fd4c4f18f53b78bc743691
Makarios <makarios.fared@gmail.com>
2024-02-18 21:08:02 +0200

    create bloc and login event

---
+++ lib/pages/blocs/auth_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_event.dart';
+
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthEvent>(
+      (event, emit) async {
+        if (event is LoginEvent) {
+          emit(LoginLoading());
+          try {
+            UserCredential user =
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: event.email,
+              password: event.password,
+            );
+            emit(LoginSuccess());
+          } on FirebaseAuthException catch (ex) {
+            if (ex.code == 'user-not-found') {
+              emit(LoginFailure(errMessage: 'user-not-found'));
+            } else if (ex.code == 'wrong-password') {
+              emit(LoginFailure(errMessage: 'Wrong password'));
+            }
+          } catch (e) {
+            emit(LoginFailure(errMessage: 'Something went wrong'));
+          }
+        }
+      },
+    );
+  }
+}
